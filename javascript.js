bbf105cccdf197273b51ff0b309455b2508b80a1
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2025-01-05 16:40:04 +0100

    place all functions in prototypes

--- javascript.js
+++ javascript.js
@@ -1,59 +1,56 @@
+// Node Declarations
 const body = document.querySelector("body");
-const bgMusic = document.querySelector("audio");
-bgMusic.muted = false;
+const zoomField = document.getElementById("zoomfield");
 
-bgMusic.removeAttribute("muted");
+let globalPrototype = Object.getPrototypeOf(
+  Object.getPrototypeOf(DivConstructor)
+);
 
-const div = document.createElement("div");
-div.style.backgroundColor = getRandomRgbValue();
-body.appendChild(div);
-
-function getRandomRgbValue() {
+//this works, and does get invoked
+globalPrototype.getRandomRgbValue = function () {
   let getValue = () => Math.floor(Math.random() * 255);
   return `rgb(${getValue()}, ${getValue()}, ${getValue()})`;
-}
-
-function DivConstructor(e) {
-  this.node = document.createElement("div");
-  this.node.style.backgroundColor = getRandomRgbValue();
-  this.node.style.display = "flex";
-  this.node.addEventListener(
-    "mouseenter",
-    (e) => {
-      if (e.target.clientHeight > e.target.clientWidth) {
-        this.node.style.flexDirection = "column";
-      }
-
-      newDiv(e);
-      newDiv(e);
-      //addDivEvent(e);
-    },
-    { once: true }
-  );
-}
-
-DivConstructor.prototype.makeNewDiv = function (e) {
-  console.log("yes");
-  console.log(e);
 };
 
-div.addEventListener("mouseenter", (e) => {
-  newDiv(e);
-});
+function DivConstructor() {
+  //creating element
+  this.div = document.createElement("div");
+  this.div.style.backgroundColor = this.getRandomRgbValue();
+  this.div.style.display = "flex";
+  let currentObject = this;
 
-function addDivEvent(e) {
-  e.target.addEventListener(
+  //adding eventlistener to trigger a new div to be made
+  this.div.addEventListener(
     "mouseenter",
-    (e) => {
-      newDiv(e);
-      newDiv(e);
+    function (e) {
+      //saved the current object as currentObject, otherwise "this" refers to a dom DOM node, since that is where the listener will be
+      currentObject.checkFlexDirection(e);
+      currentObject.make2NewDivs(e);
     },
     { once: true }
   );
 }
 
-function newDiv(e) {
-  console.log(e.target);
-  let newFrame = new DivConstructor();
-  e.target.appendChild(newFrame.node);
-}
+//this is the first DIV being applied, which has an eventlistener to split into 2 new divs
+zoomField.appendChild(new DivConstructor().div);
+
+Object.assign(DivConstructor.prototype, {
+  checkFlexDirection: function (e) {
+    if (e.target.clientHeight > e.target.clientWidth) {
+      this.div.style.flexDirection = "column";
+    }
+  },
+
+  make2NewDivs: function (e) {
+    this.newDiv(e);
+    this.newDiv(e);
+  },
+
+  newDiv: function newDiv(e) {
+    let newFrame = new DivConstructor();
+    e.target.appendChild(newFrame.div);
+    if (newFrame.div.matches(":hover")) {
+    } else {
+    }
+  },
+});
